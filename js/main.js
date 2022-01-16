5f0ec052732d2fb29b35be608fccbff694ca80c1
Ryan Hill <56138705+xjjeepster@users.noreply.github.com>
2022-01-15 17:06:23 -0700

    add commas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const links = [{
     label: 'Week 1 Notes',
     url: 'week1/week1.html',
-}
+},
 {
     label: 'Week 2 Notes',
     url: 'week2/week2.html',
-}
+},
 {
     label: 'Week 2 Team',
     url: 'week2/teamW2.html',
