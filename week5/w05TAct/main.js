a71b0518c13d3e1b43b162974cc6efac76142790
Ryan Hill <56138705+xjjeepster@users.noreply.github.com>
2022-02-10 15:51:02 -0700

    update

--- week5/w05TAct/main.js
+++ week5/w05TAct/main.js
@@ -1,1 +1,44 @@
-const myhike = new hikeList('hikes');
+//create an array of hikes
+import hikeList from './hike.js';
+
+console.log(hikeList);
+
+// const myHike = new Hikes('hikeListId');
+
+const imgBasePath = "//byui-cit.github.io/cit261/examples/";
+  //on load grab the array and insert it into the page
+  window.addEventListener("load", () => {
+    showHikeList();
+  });
+  
+  function showHikeList() {
+    const hikeListElement = document.getElementById("hikes");
+    hikeListElement.innerHTML = "";
+    renderHikeList(hikeList, hikeListElement);
+  }
+  
+  function renderHikeList(hikes, parent) {
+    hikes.forEach(hike => {
+      parent.appendChild(renderOneHike(hike));
+    });
+  }
+
+
+function renderOneHike(hike) {
+  const item = document.createElement("li");
+
+  item.innerHTML = ` <h2>${hike.name}</h2>
+        <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+        <div class="info">
+                <div>
+                    <h3>Distance</h3>
+                    <p>${hike.distance}</p>
+                </div>
+                <div>
+                    <h3>Difficulty</h3>
+                    <p>${hike.difficulty}</p>
+                </div>
+        </div>`;
+
+  return item;
+}
