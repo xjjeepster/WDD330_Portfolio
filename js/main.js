f017e0238fbab3d6e9b36027e65d8b6889a57dce
Ryan Hill <56138705+xjjeepster@users.noreply.github.com>
2022-04-02 17:39:19 -0600

    update index

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const links = [{
     label: 'Week 1 Notes',
     url: 'week1/week1.html',
-}
-,
+},
 {
     label: 'Week 2 Notes',
     url: 'week2/week2.html',
@@ -29,6 +28,10 @@ const links = [{
 },
 {
     label: 'Week 5 Notes',
+    url: 'week5/w05Tact/index.html',
+},
+{
+    label: 'Week 5 Team Activity',
     url: 'week5/week5.html',
 },
 {
@@ -44,6 +47,10 @@ const links = [{
     url: 'week8/week8.html',
 },
 {
+    label: 'Week 8 Team Activity',
+    url: 'week8/w9tact/swapi.html',
+},
+{
     label: 'Week 9 Team Activity',
     url: 'week9/w09teamactivity/index-Start.html',
 },
@@ -54,6 +61,14 @@ const links = [{
 {
     label: 'Week 10 Notes',
     url: 'week10/week10.html',
+},
+{
+    label: 'Week 10 Team Activity',
+    url: 'week10/w10teamactivity/quakes.html',
+},
+{
+    label: 'Week 11 Team Activity',
+    url: 'week11/w11teamactivity/client/week11.html',
 },
 {
     label: 'Final Project',
